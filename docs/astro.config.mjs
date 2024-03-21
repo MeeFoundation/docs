87bc1958132e10e9de0c1ce6faeb7b57818b94bf
Anthony Ivanov <aivanov@swiftinvention.com>
2024-03-22 00:30:27 +0100

    logo fix

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,8 +6,8 @@ export const site = 'https://mee.foundation/';
 export const sharedConfig = {
   title: 'Mee Foundation',
   logo: {
-    light: '/src/assets/logo-light.svg',
-    dark: '/src/assets/logo-dark.svg',
+    light: '/logo-light.svg',
+    dark: '/logo-dark.svg',
     replacesTitle: true,
   }
 }
